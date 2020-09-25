de918832ddc2c137a250f7c116a0bfec72d9b83d
ydumasy <ydumasy@gmail.com>
2020-09-25 14:19:35 +0200

    Amélioration de server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,36 @@
 const http = require('http');
 const app = require('./app');
 
-app.set('port', process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
 const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
 
 server.listen(process.env.PORT || 3000);
