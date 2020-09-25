e1351a5d51eeecee328848a5114ce92431bba005
ydumasy <ydumasy@gmail.com>
2020-09-25 14:12:40 +0200

    Création de app.js et installation d'Express

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Le serveur fonctionne');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
