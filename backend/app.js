05021a2e01b1865afc21e845c8f93ef7001f779a
ydumasy <ydumasy@gmail.com>
2020-09-25 16:37:30 +0200

    Intégration de Mongoose et Dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/<dbname>?retryWrites=true&w=majority`,
+{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res) => {
    res.json({ message: 'Le serveur fonctionne avec Express' }); 
