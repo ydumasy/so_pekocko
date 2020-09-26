2571238fbea3cecfed2c246a1ea237e9f675307e
ydumasy <ydumasy@gmail.com>
2020-09-26 18:05:03 +0200

    Création de la route 'createSauce'

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,13 +1,25 @@
 const Sauce = require('../models/Sauce');
 
-exports.getAllSauces = (req, res) => {
+exports.getSauces = (req, res) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(err => res.status(400).json({ err }));
 }
 
-exports.getOneSauce = (req, res) => {
+exports.getSauceById = (req, res) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
         .catch(err => res.status(404).json({ err }));
+}
+
+exports.createSauce = (req, res) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete req.body._id;
+    const sauce = new Sauce({
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    sauce.save()
+        .then(() => res.status(201).json({ message: "Nouvelle sauce créée" }))
+        .catch(err => res.status(400).json({ err }));
 }
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const saucesCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const router = express.Router();
 
-router.get('/', auth, saucesCtrl.getAllSauces);
-router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.get('/', auth, saucesCtrl.getSauces);
+router.get('/:id', auth, saucesCtrl.getSauceById);
+router.post('/', auth, multer, saucesCtrl.createSauce);
 
 module.exports = router;
